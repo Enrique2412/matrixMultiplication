8116c30bce07472a3a2c19be232802d37f8b35b7
Enrique2412 <46177459+Enrique2412@users.noreply.github.com>
2019-03-27 17:43:10 -0600

    Update matrixMultiplication.java
    The ultimate and final version

--- matrixMultiplication.java
+++ matrixMultiplication.java
@@ -1,5 +1,6 @@
+package matrixMultiplication;
 import java.util.*;
-public class ValdiviaEnrique6E_Exam2{
+public class matrixMultiplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,7 +10,7 @@ public class ValdiviaEnrique6E_Exam2{
 		int fil1,col1,fil2,col2;
 		int i,j,x;
 		int f = 0;
-		int option;
+		int option, correct1, correct2;
 		double matrix1 [][] = null;
 		double matrix2 [][] = null;
 		double matrix3 [][] = null;
@@ -62,10 +63,65 @@ public class ValdiviaEnrique6E_Exam2{
 						 		  System.out.println(" ");
 						 	  }
 						 	  System.out.println("\n");                                             
-		    				 System.out.println("");												//First matrix
+		    				
+		    				System.out.println("Are the values of the matrix correct ?");
+		 					System.out.println("1.-Yes");
+		 					System.out.println("2.-No");
+		 					correct1  = sc.nextInt();
+			 					do {
+									switch (correct1)
+									 {
+									  case 2:
+											  System.out.println("Rewrite the values: ");
+													  for (i = 0; i<fil1;i++)
+														{
+															for(j = 0;j<col1;j++)
+															{
+																System.out.print("Position: " + i + ","+j + " ");
+																matrix1[i][j] = sc.nextDouble();
+																
+															}
+														}
+													  System.out.println("\n"); 
+														System.out.println("The matrix is: ");
+														for (i = 0; i<fil1;i++)
+														{
+															for(j = 0;j<col1;j++)
+															{
+																System.out.print(matrix1[i][j]+"\t");
+															}
+															System.out.println("");
+														}
+														
+														System.out.println("Are the values of the matrix correct ?");
+														System.out.println("1.-Yes");
+														System.out.println("2.-No");
+														correct1 = sc.nextInt();
+										  break;
+										  
+									case 1:
+									  System.out.println("They are correct");		
+									  correct1 = 1;
+						           break;
+						           
+						           default:
+						        	   System.out.println("That is not an option");
+						        	   correct1 = 2;
+						        	   System.out.println("Are the values of the matrix correct ?");
+										System.out.println("1.-Yes");
+										System.out.println("2.-No");
+										correct1 = sc.nextInt();
+						           break;
+						           
+						           
+					            }
+					}while (correct1 != 1);	                                                          //First matrix
+			    	 System.out.println("\n");			 
+			    				 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	  
+		 					
 		    				 
-		    				 
-		    				 matrix2 = new double [fil2][col2];                                      //Second matrix
+		    		 matrix2 = new double [fil2][col2];                                      //Second matrix
 		     				System.out.println("Give me the numbers of matrix 2: ");
 		     				for (i=0; i<fil2; i++)
 		     				{
@@ -89,6 +145,64 @@ public class ValdiviaEnrique6E_Exam2{
 							 	  System.out.println("\n");                                             
 		     				 System.out.println("");                                              //Second matrix
 		     				 
+		     				 
+		     				System.out.println("Are the values of the matrix correct ?");
+							System.out.println("1.-Yes");
+							System.out.println("2.-No");
+							correct2 = sc.nextInt();
+		 					do {
+										switch (correct2)
+										 {
+										  case 2:
+												  System.out.println("Rewrite the values: ");
+														  for (i = 0; i<fil2;i++)
+															{
+																for(j = 0;j<col2;j++)
+																{
+																	System.out.print("Position: " + i + ","+j + " ");
+																	matrix2[i][j] = sc.nextDouble();
+																	
+																}
+															}
+														  System.out.println("\n"); 
+															System.out.println("The second matrix is: ");
+															for (i = 0; i<fil2;i++)
+															{
+																for(j = 0;j<col2;j++)
+																{
+																	System.out.print(matrix2[i][j]+"\t");
+																}
+																System.out.println("");
+															}
+															
+															System.out.println("Are the values of the matrix correct ?");
+															System.out.println("1.-Yes");
+															System.out.println("2.-No");
+															correct2 = sc.nextInt();
+											  break;
+										case 1:
+										  System.out.println("They are correct");		
+										  correct2 = 1;
+							           break;
+							           
+							           default:
+							        	   System.out.println("That is not an option");
+							        	   correct2 = 2;
+							        	   System.out.println("Are the values of the matrix correct ?");
+											System.out.println("1.-Yes");
+											System.out.println("2.-No");
+											correct2 = sc.nextInt();
+							           break;
+					           
+							         }
+						}while (correct2 != 1);	                                                         
+				    	 System.out.println("\n");	
+				     				 
+		     				 
+		     				 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	     		////////////Multiplication 		 
+		     			
+							
 		     				 System.out.println("The result of the multiplication is: ");         ////////////Multiplication 
 
 		     				matrix3 = new double [fil1][col2];
@@ -103,17 +217,17 @@ public class ValdiviaEnrique6E_Exam2{
 		     						System.out.print(matrix3[i][j]+"\t");
 		     					  }
 		     					System.out.println("");
-		     				  }
-		     				System.out.println("\n");
-		     		}
-			   else 
-			    System.out.println("It can not be solved \nThe number of columns and rows exceeds the limit \nOr The number of columns in matrix 1 is not the same as the number of rows in matrix 2");
+		     				    }
+		     				    System.out.println("\n");
+		     		    }
+			                      else 
+			                    	  System.out.println("It can not be solved \nThe number of columns and rows exceeds the limit \nOr The number of columns in matrix 1 is not the same as the number of rows in matrix 2 \n");
          		break;	
 				case 2:
 			        f=1;
 			    break;
 			    default:
-			      System.out.println("Select a real option");
+			      System.out.println("Select a real option \n");
 			     break;
 			     
 		   }
