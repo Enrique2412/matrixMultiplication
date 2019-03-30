787e98c37e5f35e0e612a5bd982b463b736f7ffe
Enrique2412 <46177459+Enrique2412@users.noreply.github.com>
2019-03-29 18:47:31 -0600

    Update matrixMultiplication.java
    Final final version

--- matrixMultiplication.java
+++ matrixMultiplication.java
@@ -1,6 +1,5 @@
-package matrixMultiplication;
 import java.util.*;
-public class matrixMultiplication {
+public class ValdiviaEnrique6E_Exam2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -30,7 +29,7 @@ public class matrixMultiplication {
 			switch(option)
 			{
 			 case 1:
-        			System.out.println("Please enter the number of row for matrix 1: ");
+        			System.out.println("Please enter the number of rows for matrix 1: ");
         			fil1 = sc.nextInt();
         			System.out.println("Please enter the number of columns for matrix 1: ");
         			col1 = sc.nextInt();
@@ -41,187 +40,220 @@ public class matrixMultiplication {
     			
 				if ((col1 == fil2)&&(fil1 <= 10)&&(col1<=10)&&(fil2<=10)&&(col2<=10))
 			    {
-		        	 matrix1 = new double [fil1][col1];                                      //First matrix
-		    				System.out.println("Give me the numbers of matrix 1: ");
-		    				for (i=0;i<fil1;i++)
-		    				{
-		    					for(j=0;j<col1;j++)
-		    					{
-		    						System.out.print("Position " + i + ","+j+": ");
-		    						matrix1[i][j] = sc.nextDouble();
-		    					}
-		    				}
-		    				
-		    				System.out.println("The first matrix is:");
-						 	  for (i=0;i<fil1;i++)
-						 	  {
-						 		  for (j=0;j<col1;j++)
-						 		  {
-						 			  System.out.print(matrix1[i][j]+"\t");
-						 			  
-						 		  }
-						 		  System.out.println(" ");
-						 	  }
-						 	  System.out.println("\n");                                             
-		    				
-		    				System.out.println("Are the values of the matrix correct ?");
-		 					System.out.println("1.-Yes");
-		 					System.out.println("2.-No");
-		 					correct1  = sc.nextInt();
-			 					do {
-									switch (correct1)
-									 {
-									  case 2:
-											  System.out.println("Rewrite the values: ");
-													  for (i = 0; i<fil1;i++)
-														{
-															for(j = 0;j<col1;j++)
-															{
-																System.out.print("Position " + i + ","+j + ": ");
-																matrix1[i][j] = sc.nextDouble();
-																
-															}
-														}
-													  System.out.println("\n"); 
-														System.out.println("The matrix is: ");
-														for (i = 0; i<fil1;i++)
-														{
-															for(j = 0;j<col1;j++)
-															{
-																System.out.print(matrix1[i][j]+"\t");
-															}
-															System.out.println("");
-														}
-														
-														System.out.println("Are the values of the matrix correct ?");
+					if (((fil1 <= 0)||(col1 <= 0)||(fil2 <= 0)||(col2 <= 0)))
+					{
+						System.out.println("It can not be solved, dimensions less or equal to cero");
+					}else 
+					  {
+						        	 matrix1 = new double [fil1][col1];                                      //First matrix
+						    				System.out.println("Give me the numbers of matrix 1: ");
+						    				for (i=0;i<fil1;i++)
+						    				{
+						    					for(j=0;j<col1;j++)
+						    					{
+						    						System.out.print("Position " + i + ","+j+": ");
+						    						matrix1[i][j] = sc.nextDouble();
+						    					}
+						    				}
+						    				
+						    				System.out.println("The first matrix is:");
+										 	  for (i=0;i<fil1;i++)
+										 	  {
+										 		  for (j=0;j<col1;j++)
+										 		  {
+										 			  System.out.print(matrix1[i][j]+"\t");
+										 			  
+										 		  }
+										 		  System.out.println(" ");
+										 	  }
+										 	  System.out.println("\n");                                             
+						    				
+						    				System.out.println("Are the values of the matrix correct ?");
+						 					System.out.println("1.-Yes");
+						 					System.out.println("2.-No");
+						 					correct1  = sc.nextInt();
+							 					do {
+													switch (correct1)
+													 {
+													  case 2:
+															  System.out.println("Rewrite the values: ");
+																	  for (i = 0; i<fil1;i++)
+																		{
+																			for(j = 0;j<col1;j++)
+																			{
+																				System.out.print("Position " + i + ","+j + ": ");
+																				matrix1[i][j] = sc.nextDouble();
+																				
+																			}
+																		}
+																	  System.out.println("\n"); 
+																		System.out.println("The matrix is: ");
+																		for (i = 0; i<fil1;i++)
+																		{
+																			for(j = 0;j<col1;j++)
+																			{
+																				System.out.print(matrix1[i][j]+"\t");
+																			}
+																			System.out.println("");
+																		}
+																		
+																		System.out.println("Are the values of the matrix correct ?");
+																		System.out.println("1.-Yes");
+																		System.out.println("2.-No");
+																		correct1 = sc.nextInt();
+														  break;
+														  
+													case 1:
+													  System.out.println("They are correct");		
+													  correct1 = 1;
+										           break;
+										           
+										           default:
+										        	   System.out.println("That is not an option");
+										        	   correct1 = 2;
+										        	   System.out.println("Are the values of the matrix correct ?");
 														System.out.println("1.-Yes");
 														System.out.println("2.-No");
 														correct1 = sc.nextInt();
-										  break;
-										  
-									case 1:
-									  System.out.println("They are correct");		
-									  correct1 = 1;
-						           break;
-						           
-						           default:
-						        	   System.out.println("That is not an option");
-						        	   correct1 = 2;
-						        	   System.out.println("Are the values of the matrix correct ?");
-										System.out.println("1.-Yes");
-										System.out.println("2.-No");
-										correct1 = sc.nextInt();
-						           break;
-						           
-						           
-					            }
-					}while (correct1 != 1);	                                                          //First matrix
-			    	 System.out.println("\n");			 
-			    				 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	  
-		 					
-		    				 
-		    		 matrix2 = new double [fil2][col2];                                      //Second matrix
-		     				System.out.println("Give me the numbers of matrix 2: ");
-		     				for (i=0; i<fil2; i++)
-		     				{
-		     					for(j=0; j<col2; j++)
-		     					{
-		     						System.out.print("Position " + i + ","+j+": ");
-		     						matrix2[i][j] = sc.nextDouble();
-		     					}
-		     				}
-		     				
-		     				System.out.println("The second matrix is:");
-							 	  for (i=0; i<fil2; i++)
-							 	  {
-							 		  for (j=0; j<col2; j++)
-							 		  {
-							 			  System.out.print(matrix2[i][j]+"\t");
-							 			  
-							 		  }
-							 		  System.out.println(" ");
-							 	  }
-							 	  System.out.println("\n");                                             
-		     				 System.out.println("");                                              //Second matrix
-		     				 
-		     				 
-		     				System.out.println("Are the values of the matrix correct ?");
-							System.out.println("1.-Yes");
-							System.out.println("2.-No");
-							correct2 = sc.nextInt();
-		 					do {
-										switch (correct2)
-										 {
-										  case 2:
-												  System.out.println("Rewrite the values: ");
-														  for (i = 0; i<fil2;i++)
-															{
-																for(j = 0;j<col2;j++)
-																{
-																	System.out.print("Position " + i + ","+j + ": ");
-																	matrix2[i][j] = sc.nextDouble();
-																	
-																}
-															}
-														  System.out.println("\n"); 
-															System.out.println("The second matrix is: ");
-															for (i = 0; i<fil2;i++)
-															{
-																for(j = 0;j<col2;j++)
-																{
-																	System.out.print(matrix2[i][j]+"\t");
-																}
-																System.out.println("");
-															}
-															
-															System.out.println("Are the values of the matrix correct ?");
-															System.out.println("1.-Yes");
-															System.out.println("2.-No");
-															correct2 = sc.nextInt();
-											  break;
-										case 1:
-										  System.out.println("They are correct");		
-										  correct2 = 1;
-							           break;
-							           
-							           default:
-							        	   System.out.println("That is not an option");
-							        	   correct2 = 2;
-							        	   System.out.println("Are the values of the matrix correct ?");
+										           break;
+										           
+										           
+									            }
+									}while (correct1 != 1);	                                                          //First matrix
+							    	 System.out.println("\n");			 
+							    				 
+					//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	  
+						 					
+						    				 
+						    		 matrix2 = new double [fil2][col2];                                      //Second matrix
+						     				System.out.println("Give me the numbers of matrix 2: ");
+						     				for (i=0; i<fil2; i++)
+						     				{
+						     					for(j=0; j<col2; j++)
+						     					{
+						     						System.out.print("Position " + i + ","+j+": ");
+						     						matrix2[i][j] = sc.nextDouble();
+						     					}
+						     				}
+						     				
+						     				System.out.println("The second matrix is:");
+											 	  for (i=0; i<fil2; i++)
+											 	  {
+											 		  for (j=0; j<col2; j++)
+											 		  {
+											 			  System.out.print(matrix2[i][j]+"\t");
+											 			  
+											 		  }
+											 		  System.out.println(" ");
+											 	  }
+											 	  System.out.println("\n");                                             
+						     				 System.out.println("");                                              //Second matrix
+						     				 
+						     				 
+						     				System.out.println("Are the values of the matrix correct ?");
 											System.out.println("1.-Yes");
 											System.out.println("2.-No");
 											correct2 = sc.nextInt();
-							           break;
-					           
-							         }
-						}while (correct2 != 1);	                                                         
-				    	 System.out.println("\n");	
-				     				 
-		     				 
-		     				 
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	     		////////////Multiplication 		 
-		     			
-							
-		     				 System.out.println("The result of the multiplication is: ");         ////////////Multiplication 
-
-		     				matrix3 = new double [fil1][col2];
-		     				for (i=0; i<fil1 ;i++)
-		     				{
-		     					for(j=0; j<col2; j++)
-		     					 {
-		     						for(x=0; x<col1; x++)
-		     						  {
-		     									matrix3 [i][j] = matrix3 [i][j] + (matrix1[i][x] * matrix2 [x][j]);
-		     						  }
-		     						System.out.print(matrix3[i][j]+"\t");
-		     					  }
-		     					System.out.println("");
-		     				    }
-		     				    System.out.println("\n");
+						 					do {
+														switch (correct2)
+														 {
+														  case 2:
+																  System.out.println("Rewrite the values: ");
+																		  for (i = 0; i<fil2;i++)
+																			{
+																				for(j = 0;j<col2;j++)
+																				{
+																					System.out.print("Position " + i + ","+j + ": ");
+																					matrix2[i][j] = sc.nextDouble();
+																					
+																				}
+																			}
+																		  System.out.println("\n"); 
+																			System.out.println("The second matrix is: ");
+																			for (i = 0; i<fil2;i++)
+																			{
+																				for(j = 0;j<col2;j++)
+																				{
+																					System.out.print(matrix2[i][j]+"\t");
+																				}
+																				System.out.println("");
+																			}
+																			
+																			System.out.println("Are the values of the matrix correct ?");
+																			System.out.println("1.-Yes");
+																			System.out.println("2.-No");
+																			correct2 = sc.nextInt();
+															  break;
+														case 1:
+														  System.out.println("They are correct");		
+														  correct2 = 1;
+											           break;
+											           
+											           default:
+											        	   System.out.println("That is not an option");
+											        	   correct2 = 2;
+											        	   System.out.println("Are the values of the matrix correct ?");
+															System.out.println("1.-Yes");
+															System.out.println("2.-No");
+															correct2 = sc.nextInt();
+											           break;
+									           
+											         }
+										}while (correct2 != 1);	                                                         
+								    	 System.out.println("\n");	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////Print both matrices
+								    	 
+															System.out.println("The first matrix is:");
+															for (i=0;i<fil1;i++)
+															{
+															  for (j=0;j<col1;j++)
+															       {
+															        System.out.print(matrix1[i][j]+"\t");
+															
+															       }
+															   System.out.println(" ");
+															}
+															System.out.println("\n");
+															
+															
+															System.out.println("The second matrix is: ");
+															for (i = 0; i<fil2;i++)
+															{
+															   for(j = 0;j<col2;j++)
+															    {
+															       System.out.print(matrix2[i][j]+"\t");
+															     }
+															   System.out.println("");
+															}
+															System.out.println("\n");
+								     				 
+						     				 
+						     				 
+					/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	     		////////////Multiplication 		 
+						     			
+											
+						     				 System.out.println("The result of the multiplication is: ");         ////////////Multiplication 
+				
+						     				matrix3 = new double [fil1][col2];
+						     				for (i=0; i<fil1 ;i++)
+						     				{
+						     					for(j=0; j<col2; j++)
+						     					 {
+						     						for(x=0; x<col1; x++)
+						     						  {
+						     									matrix3 [i][j] = matrix3 [i][j] + (matrix1[i][x] * matrix2 [x][j]);
+						     						  }
+						     						System.out.print(matrix3[i][j]+"\t");
+						     					  }
+						     					System.out.println("");
+						     				    }
+						     				    System.out.println("\n");
+					         }
+		     				    
+		     				    
 		     		    }
-			                      else 
-			                    	  System.out.println("It can not be solved \nThe number of columns and rows exceeds the limit \nOr The number of columns in matrix 1 is not the same as the number of rows in matrix 2 \n");
+		     		    else
+			            System.out.println("It can not be solved \nThe number of columns and rows exceeds the limit \nOr The number of columns in matrix 1 is not the same as the number of rows in matrix 2 \n");
          		break;	
 				case 2:
 			        f=1;
@@ -236,5 +268,3 @@ public class matrixMultiplication {
 	sc.close();
     }
 }
-
-         	
